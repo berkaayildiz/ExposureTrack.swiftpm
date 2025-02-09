5e4870f46ea9893f66ce29401cd71d3e1f53821d
Berkay Yıldız <dev@berkaayildiz.com>
2025-02-09 11:20:00 +0300

    initialize project

---
+++ ExposureTrackApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@available(iOS 17, *)
+@main
+struct ExposureTrackApp: App {    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
